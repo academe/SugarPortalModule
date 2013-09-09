fb6cc5024fb2b5cbddac07d1e4f9fc512c39f8bb
Jason Judge <jason.judge@consil.co.uk>
2013-09-09 15:16:39 +0100

    Create manifest.php

---
+++ manifest.php
@@ -0,0 +1,46 @@
+<?php
+$manifest =array (
+  'acceptable_sugar_versions' =>
+  array (
+  ),
+  'author' => 'Jason Judge',
+  'description' => '',
+  'icon' => '',
+  'is_uninstallable' => 'true',
+  'name' => 'package',
+  'published_date' => '2013-09-09 00:00:00',
+  'type' => 'module',
+  'version' => '1271971429',
+);
+$installdefs =array (
+  'id' => 'sugar2013_09_09_000000',
+  'copy' =>
+  array (
+    0 =>
+    array (
+      'from' => '<basepath>/install/SugarPortalModule/custom/service/v4_portal/registry.php',
+      'to' => 'custom/service/v4_portal/registry.php',
+    ),
+    1 =>
+    array (
+      'from' => '<basepath>/install/SugarPortalModule/custom/service/v4_portal/rest.php',
+      'to' => 'custom/service/v4_portal/rest.php',
+    ),
+    2 =>
+    array (
+      'from' => '<basepath>/install/SugarPortalModule/custom/service/v4_portal/SugarRestServiceImpl_v4_portal.php',
+      'to' => 'custom/service/v4_portal/SugarRestServiceImpl_v4_portal.php',
+    ),
+    3 =>
+    array (
+      'from' => '<basepath>/install/SugarPortalModule/custom/service/v4_portal/SugarRestServiceUtil_v4_portal.php',
+      'to' => 'custom/service/v4_portal/SugarRestServiceUtil_v4_portal.php',
+    ),
+  ),
+  'beans' =>
+  array (
+  ),
+  'logic_hooks' =>
+  array (
+  ),
+);
